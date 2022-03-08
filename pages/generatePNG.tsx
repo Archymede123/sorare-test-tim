3926e544ee13bbcd61b727c7b6b3d4b1892bc83e
timothee reynaud <reynaudtimothee@gmail.com>
2022-03-08 14:22:35 +0100

    generate PDF

---
+++ pages/generatePNG.tsx
@@ -0,0 +1,23 @@
+import type { NextPage } from "next";
+
+const CardsPage: NextPage = () => {
+  return (
+    <>
+      <h1>To generate a PNG I would :</h1>
+      <ul>
+        <li>
+          Take a few hours to try out solutions details here :
+          https://stackoverflow.com/a/46243263/12842253
+        </li>
+        <li>Discuss my findings with the team</li>
+        <li> Assess each solution (depending on the purpose and time)</li>
+        <li>
+          For a better rendering, my personnal preferences would seem to be the
+          one detailed here : https://stackoverflow.com/a/53067267/12842253
+        </li>
+      </ul>
+    </>
+  );
+};
+
+export default CardsPage;
