89cc92ef74a7d77a19ada2c009a5973b5691381e
timothee reynaud <reynaudtimothee@gmail.com>
2022-03-08 11:16:53 +0100

    exercice 1 & 2

---
+++ components/card.tsx
@@ -0,0 +1,89 @@
+import styled from "styled-components";
+import Image from "next/image";
+import { ICard } from "../types";
+
+const CardLayout = styled.div`
+  position: relative;
+  border-radius: 1rem;
+  background: rgb(34, 193, 195);
+  background: linear-gradient(
+    0deg,
+    rgba(34, 193, 195, 1) 0%,
+    rgba(253, 187, 45, 1) 100%
+  );
+  height: 400px;
+  color: white;
+
+  .cardContent {
+    height: 100%;
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    z-index: 10;
+    justify-content: center;
+    align-items: center;
+    gap: 4px;
+
+    & > * {
+      margin: 0;
+    }
+
+    .playerName {
+      font-size: 1.4rem;
+      font-weight: 700;
+      margin-bottom: 0.5rem;
+    }
+  }
+
+  .cardInfos {
+    position: absolute;
+    bottom: 0;
+    border-top: 1px solid white;
+    background: #ffffff5c;
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    width: 100%;
+    color: black;
+    font-weight: 700;
+  }
+
+  .cardImage {
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 400px;
+    width: 100%;
+    opacity: 0.5;
+  }
+`;
+
+type TCardProps = {
+  card: ICard;
+};
+
+const Card = ({ card }: TCardProps) => {
+  return (
+    <CardLayout key={card.id}>
+      <div className="cardContent">
+        <p className="playerName">{card.player.matchName}</p>
+        <p className="playerDetails">{card.position}</p>
+        <p className="playerDetails">{card.age} years old</p>
+      </div>
+      <div className="cardInfos">
+        <p>{card.rarity} card</p>
+        <p>{card.onSale ? "on sale" : "not available"}</p>
+      </div>
+      <div className="cardImage">
+        <Image
+          src={card.player.pictureUrl}
+          alt={card.name}
+          layout="fill"
+          objectFit="contain"
+        ></Image>
+      </div>
+    </CardLayout>
+  );
+};
+
+export default Card;
--- next.config.js
+++ next.config.js
@@ -3,4 +3,17 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = {
+  nextConfig,
+  images: {
+    domains: ['assets.sorare.com'],
+  },
+  async rewrites() {
+    return [
+      {
+          source: '/api/graphql',
+          destination: 'https://api.sorare.com/graphql' // Proxy to Backend - https://nextjs.org/docs/api-reference/next.config.js/rewrites
+        }
+    ]
+  }
+}
